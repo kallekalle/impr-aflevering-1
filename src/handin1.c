436bf1c90e073cac80e154ecef20aed221f005ce
Kalle <86068227+kallekalle@users.noreply.github.com>
2026-09-17 23:04:37 +0200

    første udkast

--- src/handin1.c
+++ src/handin1.c
@@ -1,4 +1,35 @@
+#include <stdio.h>
+#include <math.h>
 
-int main(void) {
+int main(void)
+{
+start:           // Her starter programmet forfra
+    int seconds; // Erklærer variablen seconds som en integer
+    char yes; // Erklærer variablen yes som en char til senere brug
+    printf("\nDette program omregner sekunder til uger, dage, timer, minutter samt resterende sekunder!\nIndtast antal sekunder herunder\n\n");
+    scanf("%d", &seconds);
+    printf("\nDu indtastede %d sekunder\n", seconds);
 
-}
+    // Erklær de resterende variabler som integers, og herefter find resten efter divisionen
+    int minutes = seconds / 60;
+    seconds = seconds % 60;
+    int hours = minutes / 60;
+    minutes = minutes % 60;
+    int days = hours / 24;
+    hours = hours % 24;
+    int weeks = days / 7;
+    days = days % 7;
+    // weeks = weeks % 52;
+
+    printf("Dette svarer til: %d uger, %d dage, %d timer, %d minutter og %d sekunder\n", weeks, days, hours, minutes, seconds);
+    printf("\nVil du prøve igen? y for ja\n");
+    scanf(" %c", &yes);
+    if (yes == 'y')
+    {
+        goto start; // Går tilbage til start
+    }
+    else
+    {
+        return 0;
+    }
+};
